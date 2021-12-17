600c04cf90a8065f63395737544aa89a7f591965
Chris Allbritton <callbritton@mac.com>
2021-12-17 05:43:55 +0000

    Adding sequencer effects

--- rage_beast.js
+++ rage_beast.js
@@ -1,8 +1,45 @@
 let actorD = item.actor;
 let level = actorD.items.getName("Barbarian").data.data.levels;
 let subClass = actorD.items.getName("Barbarian").data.data.subclass;
-
 let weapon = "";
+
+const animation = async () => {
+  let effectScale = 0.25;
+  let effectColor = "dark_red"; //Change "blue" to "dark_red" for example if you have the patreon version of JB2A
+
+  new Sequence()
+    .effect()
+    .attachTo(token) //Instead of atLocation(), we want the animation to be stuck to the token so it follows it around
+    .file(`jb2a.energy_strands.complete.${effectColor}.01`)
+    .belowTokens() //by default, the effect would be played above the tokens, we want this one to be played underneath so we can place the overlay above it
+    .scale(effectScale)
+    .persist() //This will make it contiously loop until dismissed by another bit of code we'll define underneath as the animationEnd() function.
+    .name(`${item.actor.data.name}- Rage -${token.data._id}`) //We want a unique name that will make dismissing it later on easier.
+    .effect()
+    .attachTo(token)
+    .file(`jb2a.energy_strands.overlay.${effectColor}.01`)
+    .scale(effectScale)
+    .persist()
+    .fadeOut(500)
+    .name(`${item.actor.data.name}- Rage -${token.data._id}`) //Named it the same as the previous effect so we can dismiss both effects at the same time
+    .play();
+};
+
+async function animationEnd() {
+  Sequencer.EffectManager.endEffects({
+    name: `${item.actor.data.name}- Rage -${token.data._id}`,
+  }); //When we revert the mutation, we'll call this function to dismiss the animation
+}
+
+let mCallbacks = {
+  post: animation, //Straight after the mutation, we want to execute this animation function as a callback
+};
+
+function condition(eventData) {
+  console.log(token.id, eventData.actorData.token);
+  console.log(token.id === eventData.actorData.token._id);
+  return token.id === eventData.actorData.token._id; //This condition makes sure we have the correct token to revert the mutation from
+}
 
 // Define the weapons from existing items
 let bite = game.items.getName("Form of the Beast: Bite").toObject();
@@ -113,7 +150,8 @@ const options = {
   comparisonKeys: { ActiveEffect: "label" },
   name: "Rage", //Let's give the mutation a name so when we shift-click the revert button, we'll identify the right mutation to revert
 };
-
+console.log(embedded.Item.name);
 let updates = mergeObject(rageAE, weapon);
 
-await warpgate.mutate(token.document, updates, {}, options); //Let's mutate the token document with some updates(stats changes and active effect on), callbacks(animation function) and options(name).
+await warpgate.mutate(token.document, updates, mCallbacks, options); //Let's mutate the token document with some updates(stats changes and active effect on), callbacks(animation function) and options(name).
+warpgate.event.trigger(warpgate.EVENT.REVERT, animationEnd, condition);
