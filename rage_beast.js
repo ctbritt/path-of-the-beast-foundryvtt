a2f732ed61b47dbd4aa36b39d9c7f8a4028a88c5
Chris Allbritton <callbritton@mac.com>
2021-12-17 15:38:35 +0000

    Gave pop up some style

--- rage_beast.js
+++ rage_beast.js
@@ -88,7 +88,7 @@ if (subClass == "Path of the Beast") {
       },
     ],
     title: "Which Form of the Beast?",
-    content: `<p>When you enter your rage, you can transform, revealing the bestial power within you. Until the rage ends, you manifest a natural weapon. It counts as a simple melee weapon for you, and you add your Strength modifier to the attack and damage rolls when you attack with it, as normal.</p>
+    content: `<p><img src="https://www.dndbeyond.com/avatars/thumbnails/13710/613/420/618/637402706646069197.png" height="100" style="float:left;outline:0;border:0;vertical-align:middle;margin:0px 10px">When you enter your rage, you can transform, revealing the bestial power within you. Until the rage ends, you manifest a natural weapon. It counts as a simple melee weapon for you, and you add your Strength modifier to the attack and damage rolls when you attack with it, as normal.</p>
     <p>You choose the weapon’s form each time you rage:</p>`,
   };
   weapon = await warpgate.buttonDialog(buttonData);
