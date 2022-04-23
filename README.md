09ec8ea72d7b76415c6dfad4eefd33fc5b203b34
Chris Allbritton <callbritton@mac.com>
2022-04-23 16:41:44 +0000

    total update

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
-# Path of the Beast Weapon Spawning
-A Macro to spawn proper Path of the Beast weapons for a barbarian when they rage. 
+# Path of the Beast Complete Macro
 
-Requires 
+A Macro for automating Path of the Beast weapons for a barbarian when they rage.
+
+Requires
+
 - midi-qol
 - advanced macros
 - item macro
 - warpgate (for mutation of the actor)
 - Dfreds Convenient Effects (for basic Rage application)
 - Sequencer (for the pretty effects)
+- Automated Animations (optional)
 
-You should also have items for the weapons already created in your item directory. I've included uploaded json files for the ones I use.
+# This is a total revamp and now everything, including scaling Rage damage and whether the beastial weapons are magical or not is automated with a click of a button. You no longer need separate items in your items directory, either, as the macro has the data on the items built in.
+
 ## Usage instructions
-1. Copy the rage_beast.js macro to the Item Macro on the Rage feature. Set it to trigger as a DAE Macro (`Macro.ItemMacro > Custom > @item`). Remove all other active effects from the Rage feature. This will add the appropriate weapons while Dfred's Rage handles the basic Rage effects.
-2. Create a four new items in your item directory. (It doesn't matter what you name them.) Right-click on each item and import a .json file for the rage weapons for each new item you created. 
 
-That's it! Now, when you rage, you will get the basic rage effects and, assuming your barbarian follows the Path of the Beast, you will get a popup dialog asking which kind of weapon you wish to manifest. If you don't want to manifest a weapon, just close the dialog window without choosing one. The basic rage effects will be applied but no weapon will be generated. 
+1. Download the Rage.json item
+2. Create an item in your items directory. You can name it whatever you want.
+3. Right-click on the item you just created and choose `Import Data`.
+4. Select the downloaded rage.json item from where you downloaded it.
+5. Drag the new Rage item onto your Path of the Beast Barbarian and kick ass.
+
+That's it! Now, when you rage, you will get the basic rage effects and, assuming your barbarian follows the Path of the Beast, you will get a popup dialog asking which kind of weapon you wish to manifest. If you don't want to manifest a weapon, just close the dialog window without choosing one. The basic rage effects will be applied but no weapon will be generated.
 
 If you don't follow the Path of the Beast, you will not see a pop-up dialog, and the basic Rage effects will be applied.
+
 ## Notes
-I am planning updates to include the weapons json data in the macro itself, so you don't need to import the ones I've uploaded. This will allow me to also check for proper Barbarian level and set the magic tag appropriately. (At 6th level, the rage weapons count as magical for overcoming resistance to non-magical s/b/p damage.)
-## Thanks
-Thanks to [honeybadger](https://github.com/trioderegion/warpgate) for much patient hand-holding and for writing Warpgate, as well as to [gazkhan](https://github.com/Gazkhan) for their original rage macro that inspired me to expand on this. The animation and basic structure is theirs. 
+
+This update will automatically scale the rage damage based on the barbarian levels you have, as well as correctly determine if the manifested weapons should do magical damage or not. (At 6th level, the rage weapons count as magical for overcoming resistance to non-magical s/b/p damage.)
+
+## Many thanks!
+
+Thanks to [honeybadger](https://github.com/trioderegion/warpgate) for much patient hand-holding and for writing Warpgate, as well as to [gazkhan](https://github.com/Gazkhan) for their original rage macro that inspired me to expand on this. The animation and basic structure is theirs.
--- rage_beast.js
+++ rage_beast.js
@@ -1,4 +1,3 @@
-console.log("args: ", args);
 // throat clearing to get data on token
 const lastArg = args[args.length - 1];
 let tokenD;
@@ -8,12 +7,15 @@ let item = args[1]; //passed by @item in the DAE field
 let level = tokenD.items.getName("Barbarian").data.data.levels;
 let subClass = tokenD.items.getName("Barbarian").data.data.subclass;
 let name = tokenD.data.token.name;
-let rageDamage = Math.ceil(Math.floor(level / (9 - Math.floor(level / 9)) + 2)); //Some clever math to add the correct amount, depending on the barbarian level
+let mgcProp = "";
+if (level > 5) {
+  mgcProp = true;
+}
 
 console.log("level: ", level);
 console.log("item: ", item);
 console.log("subClass: ", subClass);
-console.log("rageDamage: ", rageDamage);
+console.log("mgcProp: ", mgcProp);
 
 if (args[0] === "on") {
   // Mesages for when your barbarian gets angry. Feel free to customize.
@@ -74,12 +76,382 @@ if (args[0] === "on") {
   }
 
   // Define the weapons from existing items
-  let bite = game.items.getName("Form of the Beast: Bite").toObject();
-  let claws = game.items.getName("Form of the Beast: Claws").toObject();
-  let tail = game.items.getName("Form of the Beast: Tail").toObject();
-  let tailSwipe = game.items
-    .getName("Form of the Beast: Tail Swipe")
-    .toObject();
+  // let bite = game.items.getName("Form of the Beast: Bite").toObject();
+  // let claws = game.items.getName("Form of the Beast: Claws").toObject();
+  // let tail = game.items.getName("Form of the Beast: Tail").toObject();
+  // let tailSwipe = game.items
+  //   .getName("Form of the Beast: Tail Swipe")
+  //   .toObject();
+  let bite = {
+    name: "Form of the Beast: Bite",
+    type: "weapon",
+    img: "icons/creatures/abilities/mouth-teeth-long-red.webp",
+    data: {
+      description: {
+        value:
+          "<p><strong>Bite.</strong> Your mouth transforms into a bestial muzzle or great mandibles (your choice). It deals 1d8 + 4 piercing damage on a hit. Once on each of your turns when you damage a creature with this bite, you regain a number of hit points equal to your proficiency bonus, provided you have less than half your hit points when you hit.</p>",
+        chat: "",
+        unidentified: "",
+      },
+      source: "",
+      quantity: 1,
+      weight: 0,
+      price: 0,
+      attunement: 0,
+      equipped: true,
+      rarity: "",
+      identified: true,
+      activation: {
+        type: "action",
+        cost: 1,
+        condition: "",
+      },
+      target: {
+        value: 1,
+        width: null,
+        units: "",
+        type: "creature",
+      },
+      range: {
+        value: 5,
+        long: null,
+        units: "ft",
+      },
+      actionType: "mwak",
+      attackBonus: "0",
+      damage: {
+        parts: [["1d8[piercing]", "piercing"]],
+      },
+      weaponType: "natural",
+      baseItem: "",
+      properties: {
+        ada: false,
+        amm: false,
+        fin: false,
+        fir: false,
+        foc: false,
+        hvy: false,
+        lgt: false,
+        lod: false,
+        mgc: `${mgcProp}`,
+        rch: false,
+        rel: false,
+        ret: false,
+        sil: false,
+        spc: false,
+        thr: false,
+        two: false,
+        ver: false,
+      },
+      proficient: true,
+    },
+    effects: [],
+    flags: {
+      "midi-qol": {
+        effectActivation: false,
+        onUseMacroName: "",
+      },
+      favtab: {
+        isFavorite: true,
+      },
+      midiProperties: {
+        nodam: false,
+        fulldam: false,
+        halfdam: false,
+        rollOther: false,
+        critOther: false,
+        magicdam: `${mgcProp}`,
+        magiceffect: false,
+        concentration: false,
+        toggleEffect: false,
+      },
+    },
+  };
+  let claws = {
+    name: "Form of the Beast: Claws",
+    type: "weapon",
+    img: "icons/commodities/claws/claws-white.webp",
+    data: {
+      description: {
+        value:
+          "<p><strong>Claws.</strong> Each of your hands transforms into a claw, which you can use as a weapon if it’s empty. It deals 1d6 +4 slashing damage on a hit. Once on each of your turns when you attack with a claw using the Attack action, you can make one additional claw attack as part of the same action.</p>",
+      },
+      source: "",
+      quantity: 1,
+      weight: 0,
+      price: 0,
+      attunement: 0,
+      equipped: true,
+      rarity: "",
+      identified: true,
+      activation: {
+        type: "action",
+        cost: 1,
+        condition: "",
+      },
+      target: {
+        value: null,
+        width: null,
+        units: "",
+        type: "",
+      },
+      range: {
+        value: 5,
+        long: null,
+        units: "ft",
+      },
+      ability: "",
+      actionType: "mwak",
+      attackBonus: "0",
+      damage: {
+        parts: [["1d6[slashing]", "slashing"]],
+      },
+      weaponType: "natural",
+      baseItem: "",
+      properties: {
+        ada: false,
+        amm: false,
+        fin: false,
+        fir: false,
+        foc: false,
+        hvy: false,
+        lgt: false,
+        lod: false,
+        mgc: `${mgcProp}`,
+        rch: false,
+        rel: false,
+        ret: false,
+        sil: false,
+        spc: false,
+        thr: false,
+        two: false,
+        ver: false,
+      },
+      proficient: true,
+    },
+    effects: [],
+    flags: {
+      "midi-qol": {
+        effectActivation: false,
+      },
+      favtab: {
+        isFavorite: true,
+      },
+      midiProperties: {
+        nodam: false,
+        fulldam: false,
+        halfdam: false,
+        rollOther: false,
+        critOther: false,
+        magicdam: `${mgcProp}`,
+        magiceffect: false,
+        concentration: false,
+        toggleEffect: false,
+      },
+    },
+  };
+  let tail = {
+    name: "Form of the Beast: Tail",
+    type: "weapon",
+    img: "icons/commodities/biological/tail-puff-grey.webp",
+    data: {
+      description: {
+        value:
+          "<p><strong>Tail.</strong> You grow a lashing, spiny tail, which deals 1d8+4 piercing damage on a hit and has the reach property. If a creature you can see within 10 feet of you hits you with an attack roll, you can use your reaction to swipe your tail and roll a d8, applying a bonus to your AC equal to the number rolled, potentially causing the attack to miss you.</p>",
+      },
+      source: "",
+      quantity: 1,
+      weight: 0,
+      price: 0,
+      attunement: 0,
+      equipped: true,
+      rarity: "",
+      identified: true,
+      activation: {
+        type: "action",
+        cost: 1,
+        condition: "",
+      },
+      target: {
+        value: 1,
+        width: null,
+        units: "",
+        type: "creature",
+      },
+      range: {
+        value: 10,
+        long: null,
+        units: "ft",
+      },
+      ability: "",
+      actionType: "mwak",
+      attackBonus: "0",
+      damage: {
+        parts: [["1d8[piercing]", "piercing"]],
+      },
+      weaponType: "natural",
+      baseItem: "",
+      properties: {
+        ada: false,
+        amm: false,
+        fin: false,
+        fir: false,
+        foc: false,
+        hvy: false,
+        lgt: false,
+        lod: false,
+        mgc: `${mgcProp}`,
+        rch: false,
+        rel: false,
+        ret: false,
+        sil: false,
+        spc: false,
+        thr: false,
+        two: false,
+        ver: false,
+      },
+      proficient: true,
+    },
+    effects: [],
+    flags: {
+      "midi-qol": {
+        effectActivation: false,
+      },
+      favtab: {
+        isFavorite: true,
+      },
+      midiProperties: {
+        nodam: false,
+        fulldam: false,
+        halfdam: false,
+        rollOther: false,
+        critOther: false,
+        magicdam: `${mgcProp}`,
+        magiceffect: false,
+        concentration: false,
+        toggleEffect: false,
+      },
+    },
+  };
+  let tailSwipe = {
+    name: "Form of the Beast: Tail Swipe",
+    type: "weapon",
+    img: "icons/commodities/biological/tail-puff-grey.webp",
+    data: {
+      description: {
+        value:
+          "<p><strong>Tail.</strong> You grow a lashing, spiny tail, which deals 1d8 piercing damage on a hit and has the reach property. If a creature you can see within 10 feet of you hits you with an attack roll, you can use your reaction to swipe your tail and roll a d8, applying a bonus to your AC equal to the number rolled, potentially causing the attack to miss you.</p>",
+      },
+      source: "",
+      quantity: 1,
+      weight: 0,
+      price: 0,
+      attunement: 0,
+      equipped: true,
+      rarity: "",
+      identified: true,
+      activation: {
+        type: "reaction",
+        cost: 1,
+        condition: "",
+      },
+      target: {
+        value: null,
+        width: null,
+        units: "",
+        type: "self",
+      },
+      range: {
+        value: null,
+        long: null,
+        units: "self",
+      },
+      ability: null,
+      actionType: "",
+      attackBonus: 0,
+      chatFlavor: "",
+      damage: {
+        parts: [],
+      },
+      weaponType: "natural",
+      baseItem: "",
+      properties: {
+        ada: false,
+        amm: false,
+        fin: false,
+        fir: false,
+        foc: false,
+        hvy: false,
+        lgt: false,
+        lod: false,
+        mgc: `${mgcProp}`,
+        rch: false,
+        rel: false,
+        ret: false,
+        sil: false,
+        spc: false,
+        thr: false,
+        two: false,
+        ver: false,
+      },
+      proficient: true,
+    },
+    effects: [
+      {
+        _id: "OuMZ5cfaCfJDQpJA",
+        changes: [
+          {
+            key: "data.attributes.ac.bonus",
+            mode: 2,
+            value: "[[+1d8]]",
+            priority: "20",
+          },
+        ],
+        disabled: false,
+        duration: {
+          startTime: null,
+        },
+        icon: "systems/dnd5e/icons/items/inventory/monster-tail.jpg",
+        label: "Form of the Beast: Tail Swipe",
+        transfer: false,
+        flags: {
+          core: {
+            statusId: "",
+          },
+          dae: {
+            stackable: "none",
+            macroRepeat: "none",
+            specialDuration: ["1Reaction"],
+            transfer: false,
+            durationExpression: "",
+          },
+          "dnd5e-helpers": {
+            "rest-effect": "Ignore",
+          },
+          ActiveAuras: {
+            isAura: false,
+            aura: "None",
+            radius: null,
+            alignment: "",
+            type: "",
+            ignoreSelf: false,
+            height: false,
+            hidden: false,
+            hostile: false,
+            onlyOnce: false,
+            displayTemp: false,
+          },
+        },
+        tint: null,
+        selectedKey: "data.attributes.ac.bonus",
+      },
+    ],
+    flags: {
+      favtab: {
+        isFavorite: true,
+      },
+    },
+  };
 
   // Check if correct subclass is in place
   if (subClass == "Path of the Beast") {
@@ -150,10 +522,9 @@ if (args[0] === "on") {
 
   // merge updates for spawning
   let updates = weapon.update;
-  // let updates = mergeObject(rageAE, weapon.update);
+  // let updates = mergeObject(rageAE, weapon.update); << old version
 
   await warpgate.mutate(token.document, updates, mCallbacks, options); //Let's mutate the token document with some updates(stats changes and active effect on), callbacks(animation function) and options(name).
-  // warpgate.event.trigger(warpgate.EVENT.REVERT, animationEnd, condition);
 }
 
 if (args[0] === "off") {
